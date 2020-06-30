3a78bdcf565b6e66fd0e23c7829e0338e4af01d7
GuRuGu <peter.k.nets@gmail.com>
2020-06-30 15:55:19 +0300

    Rewrite exercise method

--- about_functions/exercise.rb
+++ about_functions/exercise.rb
@@ -1,3 +1,7 @@
-def return_number(a)
-  ((a / 2)**2) * (65 - (a / 2))
+age_by_two = (a / 2)
+child_bank_account = (age_by_two**2)
+child_age = (65 - age_by_two)
+
+def retirement_money(a)
+  child_bank_account * child_age
 end
