417e38c3da2a1b6e49e9718ed726f9e41d84f24b
GuRuGu <peter.k.nets@gmail.com>
2020-07-02 11:17:16 +0300

    Revert "Add gitignore & update my_each method"
    This reverts commit 47e62c120e5f65e6b8ab2dbbfa11a9a73eed24f3.

--- enumerables/enumerables.rb
+++ enumerables/enumerables.rb
@@ -1,12 +1,10 @@
-require "byebug"
-
 class Array
   def my_each(&prc)
     idx = 0
     length = self.length
 
     while idx < length
-      prc.call(self[idx])
+      self[idx] = prc.call(self[idx])
       idx += 1
     end
     
@@ -55,26 +53,5 @@ class Array
     end
 
     true
-  end
-
-  def my_flatten
-    self.my_any? { |ele| ele.kind_of?(Array) }
-    # debugger
-    # if any_arrays
-    #   current = []
-
-    #   self.my_each do |ele|
-    #     if !ele.kind_of?(Array)
-    #       current << ele
-    #     else
-    #       current.concat(ele.my_flatten)
-    #     end
-    #   end
-
-    #   current
-    # else
-    #   return self
-    # end
-    self
   end
 end
