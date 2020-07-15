01f9e57574866a86461579b2b0ae3c298d5f9e50
GuRuGu <peter.k.nets@gmail.com>
2020-07-15 15:33:57 +0300

    Set up dummy methods inside Game class

--- ghost/Game.rb
+++ ghost/Game.rb
@@ -5,6 +5,21 @@ class Game
     @dictionary = IO.readlines("dictionary.txt", chomp: true).map!(&:to_sym)
   end
 
+  def current_player
+  end
+  
+  def previous_player
+  end
+
+  def next_player!
+  end
+
+  def take_turn(player)
+  end
+
+  def valid_play?(string)
+  end
+
   def play_round
   end
 end
