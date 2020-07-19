ce7fcfefcd2cf947735d949173787201cf8d2b64
GuRuGu <peter.k.nets@gmail.com>
2020-07-19 14:34:19 +0300

    Implement some Player methods

--- ghost/Player.rb
+++ ghost/Player.rb
@@ -3,9 +3,28 @@ class Player
     @name = name
   end
 
+  def is_valid_input?(input)
+    input.kind_of?(String) && input.length == 1
+  end
+
   def guess
+    p "Player #{@name}"
+    p "Please enter a character:"
+
+    input = gets.chomp
+
+    if !self.is_valid_input?(input)
+      p "Invalid input, please enter only one character..."
+      p "-------------------------------------------------"
+      return self.guess
+    end
+
+    input
   end
 
   def alert_invalid_guess 
+    p "There is no such word in dictionary"
+    p "Good luck on your next turn!"
+    p "-------------------------------------------------"
   end
 end
