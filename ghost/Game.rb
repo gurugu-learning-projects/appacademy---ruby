4a4d187feea10a1ed7f1d880924d29d680306f8b
GuRuGu <peter.k.nets@gmail.com>
2020-07-20 11:48:53 +0300

    Rewrite some methods

--- ghost/Game.rb
+++ ghost/Game.rb
@@ -6,7 +6,8 @@ class Game
   def initialize(player_1_name, player_2_name)
     @players = [Player.new(player_1_name), Player.new(player_2_name)]
     @fragment = ""
-    @dictionary = IO.readlines("dictionary.txt", chomp: true).map!(&:to_sym)
+    # @dictionary = IO.readlines("dictionary.txt", chomp: true).map!(&:to_sym)
+    @dictionary = IO.readlines("dictionary.txt", chomp: true).to_set
   end
 
   def current_player
@@ -24,21 +25,26 @@ class Game
   def take_turn(player)
     guess = player.guess
 
-    if !self.valid_play?(guess)
-      player.alert_invalid_guess
-    else
+    if self.valid_play?(guess)
       @fragment = @fragment + guess
+      p "Good guess!"
+      p "Current string is: #{@fragment}"
+      p "-------------------------------------------------"
+    else
+      player.alert_invalid_guess
+      p "There is no such word in dictionary"
+      p "Good luck on your next turn!"
+      p "Current string is: #{@fragment}"
+      p "-------------------------------------------------"
     end
   end
 
   def valid_play?(string)
-    if string.kind_of?(String) && string.length == 1
-      @dictionary.each do |word|
-        regexp  = Regexp.new(@fragment + string)
+    @dictionary.each do |word|
+      regexp  = Regexp.new(@fragment + string)
 
-        if regexp === word
-          return true
-        end
+      if regexp === word
+        return true
       end
     end
       
@@ -53,8 +59,11 @@ class Game
       p "Player #{previous_player.name} loses!"
       p "GAME OVER"
       return
+    else
+      p "playing round..."
     end
 
+    p "moving along...q"
     self.next_player!
     self.play_round
   end
