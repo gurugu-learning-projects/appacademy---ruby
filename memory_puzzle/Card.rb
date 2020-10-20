0b7274ba3cb2165bdc64662d05408a8025af7f38
GuRuGu <peter.k.nets@gmail.com>
2020-10-20 18:20:10 +0300

    Update Card as per solution

--- memory_puzzle/Card.rb
+++ memory_puzzle/Card.rb
@@ -1,28 +1,42 @@
 class Card
-  def initialize(face)
-    @face = face
-    @isFaceUp = false
-  end
+  VALUES = ("A".."Z").to_a
 
-  def show_card
-    if self.isFaceUp
-      return self.face
+  attr_reader :value
+
+  def self.shuffled_pairs(num_pairs)
+    values = VALUES
+
+    while num_pairs > values.length
+      values = values + values
     end
 
-    return ""
+    values = values.shuffle.take(num_pairs) * 2
+    values.shuffle!
+    values.map { |val| self.new(val) }
   end
 
-  def reveal
-    self.isFaceUp = true
+  def initialize(value, revealed = false)
+    @value = value
+    @revealed = revealed
   end
 
   def hide
-    self.isFaceUp = false
+    @revealed = false
   end
 
   def to_s
+    self.revealed? ? @value.to_s : " "
   end
 
-  def ==
+  def reveal
+    @revealed = true
+  end
+
+  def revealed?
+    @revealed
+  end
+
+  def ==(object)
+    object.is_a?(self.class) && object.value == value
   end
 end
